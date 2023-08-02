232f05770ae6754e9530bc2bb5b8272899fa775f
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-08-02 21:06:55 +0200

    background-detect-didomi-uses[1]

--- background.js
+++ background.js
@@ -1,2 +1,33 @@
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete' && tab.status === 'complete') {
+    // Ejecutamos el script en la página usando la API chrome.scripting.executeScript
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: checkForDidomiHostElement
+    });
+  }
+});
 
-console.log('La extensión esta funcionando');
+
+// Esta función se ejecutará en el contexto de la página web
+function checkForDidomiHostElement() {
+  // Verificamos si la página contiene un elemento con el ID "didomi-host"
+  if (document.getElementById('didomi-host')) {
+    console.log('La página contiene un elemento con el ID "didomi-host"');
+    console.log(document.getElementById('didomi-host'));
+    console.log(document.getElementById('didomi-popup'));
+    if(document.getElementById('didomi-popup')){
+      console.log('La página puede contener un popup en el medio"');
+    }else if(document.getElementById('didomi-notice')){
+      console.log('La página puede contener un popup arriba o abajo');
+    }
+    console.log('AHHHH');
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: checkExistModal
+    });
+  }
+}
+function checkExistModal() {
+    console.log('AAAAAAAAAAAAAAAA');
+}
