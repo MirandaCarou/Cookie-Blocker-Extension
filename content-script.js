2fcba9534834ae277ee331057c9d509b697357c5
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-08-02 23:20:30 +0200

    creation-inicial-test-extension-[Accept All Cookies Funcionality implemented only for didome]]

--- content-script.js
+++ content-script.js
@@ -1,18 +1,27 @@
-
+function checkTypeDidomeModal() {
+    if(document.getElementById('didomi-popup')){
+        console.log('La página puede contener un popup en el medio"');
+        return 'didomi-popup';
+    }else if(document.getElementById('didomi-notice')){
+        console.log('La página puede contener un popup arriba o abajo');
+        return 'didomi-notice';
+    }
+}
+function configurateCookies(typeModal) {
+    console.log(typeModal);
+    if(document.getElementById('didomi-notice-agree-button')){
+        var acceptAllButton = document.getElementById('didomi-notice-agree-button');
+        acceptAllButton.click();
+    }
+}
 function checkForCookiePrefencesElement() {
     // Verificamos si la página contiene un elemento con el ID "didomi-host"
     if (document.getElementById('didomi-host')) {
-      checkTypeDidomeModal()  
+      var TypeDidodmeModal = checkTypeDidomeModal(); 
+      configurateCookies(TypeDidodmeModal);
     }
+  }
   
-  }
-  function checkTypeDidomeModal() {
-    if(document.getElementById('didomi-popup')){
-        console.log('La página puede contener un popup en el medio"');
-    }else if(document.getElementById('didomi-notice')){
-        console.log('La página puede contener un popup arriba o abajo');
-    }
-  }
 
   checkForCookiePrefencesElement();
   
