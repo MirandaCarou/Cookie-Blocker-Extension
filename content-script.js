ac8a48598b8f6bff472337521643882982fe9c84
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-08-03 19:58:42 +0200

    add  intention to comincate 2 component by messages

--- content-script.js
+++ content-script.js
@@ -7,15 +7,17 @@ function configurateCookies(privacyApli) {
             if(option == "accept" && document.getElementById('didomi-notice-agree-button')){
                 document.getElementById('didomi-notice-agree-button').click();
                 console.log('Se han aeptado todas las cookies'); 
-            }else if(option == "denyAll"){
+            }else if(option == "denyAll" && document.getElementById('didomi-notice-learn-more-button')){
+                document.getElementById('didomi-notice-learn-more-button').click();
                 checkConfigurationOptions();
             }
             break;
         case "onetrust":
             if(option == "accept" && document.getElementById('onetrust-accept-btn-handler')){
                 document.getElementById('onetrust-accept-btn-handler').click();
-                console.log('Se han aeptado todas las cookies'); 
-            }else if(option == "denyAll"){
+                console.log('Se han aceptado todas las cookies'); 
+            }else if(option == "denyAll" && document.getElementById('onetrust-pc-btn-handler')){
+                document.getElementById('onetrust-pc-btn-handler').click();
                 checkConfigurationOptions();
             }
             break;    
@@ -30,13 +32,22 @@ function checkForCookiePrefencesElement() {
       console.log('Se ha detectado el uso de el gestor de privacidad DIDOMI');  
       configurateCookies("didomi",option);
     }else if(document.getElementById('onetrust-consent-sdk')){
-        console.log('Se ha detectado el uso de el gestor de privacidad ONETRUST');  
+      console.log('Se ha detectado el uso de el gestor de privacidad ONETRUST');  
       configurateCookies("onetrust",option);  
     }else{
 
     }
-  }
-  
-  var option = "accept";
+}
+
+    chrome.runtime.sendMessage({ message: "getAcceptedStatus" }, function (response) {
+        console.log('Se va a solicitar el valor');
+        const accepted = response.accepted;
+        console.log("Valor recibido desde popup.js:", accepted);
+
+        var option = accepted ? "accept" : "denyAll";
+        checkForCookiePrefencesElement(option);
+    });
+
+
   checkForCookiePrefencesElement(option);
   
