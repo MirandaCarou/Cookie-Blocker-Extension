331bc7eaadd44d0e0f551d8db5700d3c8ed9eb55
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-08-15 19:11:14 +0200

    optimized deny/acept All extension

--- content-script.js
+++ content-script.js
@@ -1,26 +1,60 @@
-
-
-function checkConfigurationOptions(){
-    console.log("Se quieren rechazar todas las cookies")
+async function clickElementWithId(id) {
+    waitForElementsById(id)
+    .then(() => {
+      document.getElementById(id).click();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+async function clickElementWithClassName(className) {
+    waitForElementsByClass(className)
+    .then(() => {
+      document.getElementsByClassName(className)[0].click();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+async function checkConfigurationOptions(privacyApli){
+    console.log("Se quieren rechazar todas las cookies");
+        switch(privacyApli) {
+            case "didomi":
+                 clickElementWithClassName(['didomi-components-button didomi-button didomi-button-standard standard-button']);
+                 clickElementWithClassName(['didomi-components-button didomi-button didomi-button-standard standard-button']);
+                 clickElementWithClassName(['didomi-components-button didomi-button didomi-button-standard standard-button']);
+                break;
+            case "onetrust":
+                if(document.getElementsByClassName('ot-pc-refuse-all-handler')){
+                    clickElementWithClassName(['ot-pc-refuse-all-handler']);
+                }
+                if(document.getElementsByClassName('save-preference-btn-handler onetrust-close-btn-handler')){
+                    clickElementWithClassName(['save-preference-btn-handler onetrust-close-btn-handler']);
+                }
+                break;
+            default:
+                break;
+        }
+        console.log("Se han rechazado todas las cookies");
 }
 function configurateCookies(privacyApli) {
     switch (privacyApli) {
         case "didomi":
-            if(preferences == "acceptAll" && document.getElementById('didomi-notice-agree-button')){
-                document.getElementById('didomi-notice-agree-button').click();
+            if(preferences == "acceptAll"){
+                clickElementWithId(['didomi-notice-agree-button']);
                 console.log('Se han aeptado todas las cookies'); 
-            }else if(preferences == "denyAll" && document.getElementById('didomi-notice-learn-more-button')){
-                document.getElementById('didomi-notice-learn-more-button').click();
-                checkConfigurationOptions();
+            }else if(preferences == "denyAll"){
+                clickElementWithId(['didomi-notice-learn-more-button']);
+                checkConfigurationOptions(privacyApli);
             }
             break;
         case "onetrust":
-            if(preferences == "acceptAll" && document.getElementById('onetrust-accept-btn-handler')){
-                document.getElementById('onetrust-accept-btn-handler').click();
+            if(preferences == "acceptAll"){
+                clickElementWithId(['onetrust-accept-btn-handler']);
                 console.log('Se han aceptado todas las cookies'); 
-            }else if(preferences == "denyAll" && document.getElementById('onetrust-pc-btn-handler')){
-                document.getElementById('onetrust-pc-btn-handler').click();
-                checkConfigurationOptions();
+            }else if(preferences == "denyAll"){
+                clickElementWithId(['onetrust-pc-btn-handler']);
+                checkConfigurationOptions(privacyApli);
             }
             break;    
         default:
@@ -52,7 +86,53 @@ function checkForCookiePreferences(){
     });
     
 }
+function waitForElementsById(elementIds) {
+    return new Promise((resolve) => {
+      const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+          for (const elementId of elementIds) {
+            const targetElement = document.getElementById(elementId);
+            if (targetElement) {
+              observer.disconnect();
+              resolve();
+            }
+          }
+        }
+      });
+  
+      observer.observe(document, { childList: true, subtree: true });
+    });
+  }
 
-checkForCookiePreferences();
+  function waitForElementsByClass(elementClasses) {
+    return new Promise((resolve) => {
+      const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+          for (const elementClass of elementClasses) {
+            const targetElements = document.getElementsByClassName(elementClass);
+            if (targetElements.length > 0) {
+              observer.disconnect();
+              resolve();
+            }
+          }
+        }
+      });
+      observer.observe(document, { childList: true, subtree: true });
+    });
+  }
+  
+  
+  waitForElementsById(['didomi-host', 'onetrust-consent-sdk'])
+    .then(() => {
+      console.log("Los elementos han aparecido. Ejecutando comandos...");
+      checkForCookiePreferences();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+    
+
+
 
   
