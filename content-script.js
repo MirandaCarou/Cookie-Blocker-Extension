b2a20ff2d8532ac0462e5ae0f335bd2c36a57d79
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-09-12 22:44:40 +0200

    [First final version without structure]

--- content-script.js
+++ content-script.js
@@ -1,58 +1,80 @@
-async function clickElementWithId(id) {
-    waitForElementsById(id)
+function clickElementWithId(id) {
+  if(document.getElementById(id)){
+    document.getElementById(id).click();
+  }else{
+    waitForElementById(id)
     .then(() => {
       document.getElementById(id).click();
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+  }
 }
-async function clickElementWithClassName(className) {
-    waitForElementsByClass(className)
+function clickElementWithClassName(className) {
+  if(document.getElementsByClassName(className)[0] !== undefined && document.getElementsByClassName(className)[0] != null){
+    console.log(document.getElementsByClassName(className)[0].click());
+    document.getElementsByClassName(className)[0].click();
+  }else{
+    waitForElementByClass(className)
     .then(() => {
       document.getElementsByClassName(className)[0].click();
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+  }
+    
 }
 async function checkConfigurationOptions(privacyApli){
-    console.log("Se quieren rechazar todas las cookies");
         switch(privacyApli) {
             case "didomi":
-                 clickElementWithClassName(['didomi-components-button didomi-button didomi-button-standard standard-button']);
-                break;
+              if(document.getElementsByClassName('didomi-components-button didomi-button didomi-button-standard standard-button') !== undefined){
+                console.log(document.getElementsByClassName('didomi-components-button didomi-button didomi-button-standard standard-button')[0]);
+                clickElementWithClassName(['didomi-components-button didomi-button didomi-button-standard standard-button']);
+                
+              } 
+              break;
             case "onetrust":
-                if(document.getElementsByClassName('ot-pc-refuse-all-handler')){
-                    clickElementWithClassName(['ot-pc-refuse-all-handler']);
-                }
-                if(document.getElementsByClassName('save-preference-btn-handler onetrust-close-btn-handler')){
+                if(document.getElementsByClassName('save-preference-btn-handler onetrust-close-btn-handler') !== undefined){
+                    console.log('save-preference-btn-handler onetrust-close-btn-handler');
                     clickElementWithClassName(['save-preference-btn-handler onetrust-close-btn-handler']);
+                    console.log("Se le ha dado al boton de guardar preferencias");
                 }
                 break;
             default:
                 break;
         }
-        console.log("Se han rechazado todas las cookies");
+        
 }
-function configurateCookies(privacyApli) {
+async function configurateCookies(privacyApli) {
     switch (privacyApli) {
         case "didomi":
-            if(preferences == "acceptAll"){
+            if(preferences == "acceptAll" ){
                 clickElementWithId(['didomi-notice-agree-button']);
                 console.log('Se han aeptado todas las cookies'); 
             }else if(preferences == "denyAll"){
                 clickElementWithId(['didomi-notice-learn-more-button']);
-                checkConfigurationOptions(privacyApli);
+                setTimeout(() => {
+                  checkConfigurationOptions(privacyApli);
+                }, 1000);
+                
             }
             break;
         case "onetrust":
             if(preferences == "acceptAll"){
-                clickElementWithId(['onetrust-accept-btn-handler']);
-                console.log('Se han aceptado todas las cookies'); 
+                console.log('Se quieren aceptar todas las cookies'); 
+                clickElementWithId(["onetrust-accept-btn-handler"]);
             }else if(preferences == "denyAll"){
+              if(document.getElementById('onetrust-reject-all-handler')){
+                console.log('Se quieren rechazar todas las cookies sin configurar');
+                clickElementWithId(['onetrust-reject-all-handler']);
+              }else{
+                console.log('Se van a ir a configurar las cookies');
                 clickElementWithId(['onetrust-pc-btn-handler']);
-                checkConfigurationOptions(privacyApli);
+                await checkConfigurationOptions(privacyApli);
+
+              }
             }
             break;    
         default:
@@ -60,14 +82,14 @@ function configurateCookies(privacyApli) {
     }
     
 }
-function checkForCookiePrefencesElement(preferences) {
+async function checkForCookiePrefencesElement(preferences) {
     // Verificamos si la página contiene un elemento con el ID "didomi-host"
     if (document.getElementById('didomi-host')) {
       console.log('Se ha detectado el uso de el gestor de privacidad DIDOMI');  
-      configurateCookies("didomi",preferences);
+      await configurateCookies("didomi",preferences);
     }else if(document.getElementById('onetrust-consent-sdk')){
       console.log('Se ha detectado el uso de el gestor de privacidad ONETRUST');  
-      configurateCookies("onetrust",preferences);  
+      await configurateCookies("onetrust",preferences);  
     }else{
 
     }
@@ -75,16 +97,51 @@ function checkForCookiePrefencesElement(preferences) {
 
 
 var preferences = "";
-function checkForCookiePreferences(){
-    chrome.storage.sync.get("accepted", function (data) {
+async function checkForCookiePreferences(){
+    chrome.storage.sync.get("accepted", async function (data) {
         console.log("Value currently is " + data.accepted);
         preferences = data.accepted;
         console.log("The preference value is "+ preferences);
-        checkForCookiePrefencesElement(preferences);
+        await checkForCookiePrefencesElement(preferences);
     });
     
 }
-function waitForElementsById(elementIds) {
+
+async function waitForElementById(elementId) {
+  console.log(`Esperando el elemento con el ID '${elementId}'`);
+  return new Promise((resolve) => {
+    const observer = new MutationObserver((mutationsList) => {
+      const targetElement = document.getElementById(elementId);
+      console.log(targetElement);
+      if (targetElement) {
+        observer.disconnect();
+        console.log(`Se encontró el elemento con el ID '${elementId}'`);
+        resolve(targetElement);
+      } else {
+        console.log(`Aún se está esperando el elemento con el ID '${elementId}'`);
+      }
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
+  });
+}
+async function waitForElementByClass(elementClass) {
+  return new Promise((resolve) => {
+    const observer = new MutationObserver((mutationsList) => {
+      const targetElement = document.getElementsByClassName(elementClass);
+      if (targetElement) {
+        observer.disconnect();
+        resolve(targetElement);
+      } else {
+        console.log(`Aún se está esperando el elemento con la clase '${elementId}'`);
+      }
+    });
+    observer.observe(document, { childList: true, subtree: true });
+  });
+}
+
+async function waitForElementsById(elementIds) {
+  console.log('vamos a entrar en la promesa de esperar por el id');
     return new Promise((resolve) => {
       const observer = new MutationObserver((mutationsList) => {
         for (const mutation of mutationsList) {
@@ -93,6 +150,8 @@ function waitForElementsById(elementIds) {
             if (targetElement) {
               observer.disconnect();
               resolve();
+            }else{
+              console.log('Se esta esperando');
             }
           }
         }
@@ -102,15 +161,17 @@ function waitForElementsById(elementIds) {
     });
   }
 
-  function waitForElementsByClass(elementClasses) {
+  async function waitForElementsByClass(elementClasses) {
     return new Promise((resolve) => {
       const observer = new MutationObserver((mutationsList) => {
         for (const mutation of mutationsList) {
           for (const elementClass of elementClasses) {
             const targetElements = document.getElementsByClassName(elementClass);
-            if (targetElements.length > 0) {
+            if (targetElements) {
               observer.disconnect();
               resolve();
+            }else{
+              console.log('Se esta esperando');
             }
           }
         }
@@ -120,10 +181,10 @@ function waitForElementsById(elementIds) {
   }
   
   
-  waitForElementsById(['didomi-host', 'onetrust-consent-sdk'])
-    .then(() => {
+   waitForElementsById(['didomi-host', 'onetrust-consent-sdk'])
+    .then(async () => {
       console.log("Los elementos han aparecido. Ejecutando comandos...");
-      checkForCookiePreferences();
+      await checkForCookiePreferences();
     })
     .catch((error) => {
       console.error("Error:", error);
