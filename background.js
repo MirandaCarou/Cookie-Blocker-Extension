9ccef88466203fb1d53778cde45a56c561ded232
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-09-13 00:23:39 +0200

    Added coments and refiantions

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete' && tab.status === 'complete') {
-    // Ejecutamos el script en la página usando la API chrome.scripting.executeScript
     chrome.scripting.executeScript({
       target: { tabId: tabId },
       files: ['content-script.js']
     });
   }
 });
+
+// Esto se utiliza para identificar que opcion a elegido el usuario (AcceptAll o DenyAll)
 var option = "denyAll";
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("BACKGROUND.JS");
