804cfcb91e15fb84d40d44a7367890e735eceba6
Miranda <71145273+MirandaCarou@users.noreply.github.com>
2023-10-02 21:34:35 +0200

    structure-reformulation

--- Handlers/handler.js
+++ Handlers/handler.js
@@ -28,6 +28,66 @@ class Handler {
     manageSiteAsDenyAll(){
 
     };
+    clickElementWithId(id) {
+        if(document.getElementById(id)){
+          document.getElementById(id).click();
+        }else{
+          waitForElementById(id)
+          .then(() => {
+            document.getElementById(id).click();
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+        }
+      }
+    clickElementWithClassName(className) {
+        if(document.getElementsByClassName(className)[0] !== undefined && document.getElementsByClassName(className)[0] != null){
+          console.log(document.getElementsByClassName(className)[0].click());
+          document.getElementsByClassName(className)[0].click();
+        }else{
+          waitForElementByClass(className)
+          .then(() => {
+            document.getElementsByClassName(className)[0].click();
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+        }
+          
+    }
+    waitForElementsById(elementIds) {
+        return new Promise((resolve) => {
+          const observer = new MutationObserver(() => {
+              for (const elementId of elementIds) {
+                const targetElement = document.getElementById(elementId);
+                if (targetElement) {
+                  observer.disconnect();
+                  resolve();
+                }
+              }
+          });
+      
+          observer.observe(document, { childList: true, subtree: true });
+        });
+      }
+    
+     waitForElementsByClass(elementClasses) {
+        return new Promise((resolve) => {
+          const observer = new MutationObserver(() => {
+              for (const elementClass of elementClasses) {
+                const targetElements = document.getElementsByClassName(elementClass);
+                if (targetElements) {
+                  observer.disconnect();
+                  resolve();
+                }else{
+                  console.log('Se esta esperando');
+                }
+              }
+          });
+          observer.observe(document, { childList: true, subtree: true });
+        });
+      }
 }
 
 
